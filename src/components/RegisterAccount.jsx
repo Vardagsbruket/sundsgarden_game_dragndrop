864f6dc87a9a74e39f8662ddd1b19e72ed2e1eb7
MariaKim1 <mariakimpersson@gmail.com>
2023-12-02 23:56:45 +0100

    added registeraccount-page, tried to view the login and register page - no success

--- src/components/RegisterAccount.jsx
+++ src/components/RegisterAccount.jsx
@@ -1,41 +1,47 @@
 
 // ==== REGISTER ACCOUNT ====
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import Card from "./Card";
+import { useNavigate } from "react-router-dom";
+// +++++ Added const navigate on this page after seeing the Login tutorial.
+// Imported useNavigate to RegisterAccount
+// Also imported RegisterAccount and added Route to app.jsx.      
 
 
 const Register = () => {
-// Three states - one for each input field
+    // Three states - one for each input field
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-// check if the email is unique
+    // check if the email is unique
     const checkEmail = (users) => {
         const user = users.find((user) => user.email === email);
         if (user) return user;
     };
 
-// Assign this to button
+    // Assign this to button
     const handleSubmit = async () => {
         const user = await axios
         .get("/users")
         .then((res) => checkEmail(res.data, email));
 
-// if email already exists
-// else save this new user - make a post-request
+        // if email already exists
+        // else save this new user - by making a post-request
         if(user) {
             alert("User already exists!")
         } else {
             const user = {username, email, password};
             axios.post("/users", user).then(alert("User created!"));
+            navigate("../ProfilePage");
         }
     };  
 
-// added value to each state
-// added onchange to be able to update the state (from input field)
-// Also, add a handleSubmit to the button.
+    // added value to each state
+    // added onChange to be able to update the state (from input field)
+    // Also, added a handleSubmit to the button.
     return (
         <div className="container">
             <Card>
