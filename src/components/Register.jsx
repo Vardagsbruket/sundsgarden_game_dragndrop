45ad4e9842c7058be1797691f3d2b6a38327e18f
MariaKim1 <mariakimpersson@gmail.com>
2023-12-09 15:28:08 +0100

    cleaned Register - code DONE. Checked w Login seems to work.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,57 +4,38 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 import "./Register.css";
-// +++++ Added const navigate on this page after seeing the Login tutorial.
-// Imported useNavigate to RegisterAccount
-// Also imported RegisterAccount and added Route to app.jsx.
 
-const Register = () => { // Three states - one for each input field 
+//  TODO:
+//  1. does the register function work with t he login function? I THINK SO, BUT CHECK AGAIN AFTER MERGE
+//  2. Add correct navigation iin register code.
+  
+
+const Register = () => { 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
   const navigate = useNavigate();
 
-  // 1. I want to have a PW-validation - compate the two PW fields.
-  //    If they match a new user is possible. 
-  // 2. I want the user name and email to be unique.
-  //    Compare to the jsn server. If the code can't find a matching username or email then a new user is possible.
-  // 3. Submitt this function to the onClick funktion that will execute these validations.
 
-
-  const handleSubmit = async (e) => { // Assign this function to button onClick
+  const handleSubmit = async (e) => {   // Assign this function to button onClick
     e.preventDefault();
 
-      // const confirmPassword = () => { // Password validation - a match is required to create a new user.   
-        // password1 = form.password1.value;
-        // password2 = form.password2.value;
-    
-        // if (password1 === "") {
-        //   alert ("Please enter password");
-        //   return;
-        // } else if (password2 === "") {      
-        //   alert ("Please enter confirm password");
-        //   return;
-        // } else 
         if (password1 === password2) {
         
           try {
-          const response = await axios.get("http://localhost:6001/users"); // await - response
-            // console.log(response); // om det finns registrert ngt överhuvudtaget
-            // console.log(response.data); // hittar "data" via console.log
-            const users = response.data //users lista = response.data
-
-          // const checkUser = (users) => { // specificerar vad jag kollar efter - check if the email and username is unique
-            const user = users.find((user) => user.email === email | user.username === username);
+          const response = await axios.get("http://localhost:6001/users"); 
+            const users = response.data     //list of users (json) = response.data
+            const user = users.find((user) => user.email === email | user.username === username); // check if email and/or username already exists
             
-          if (user) {
+          if (user) {       // if - there is a matching user alert
             alert("Username or email already exists. Please try again!");
-          } else {
+          } else {          // else - create newUser
             console.log("i else satsen")
             const newUser = { username, email, password: password1 };
-              axios.post("http://localhost:6001/users", newUser)  // "when this promise (API) is fullfilled.."
-              .then(() => alert("User created!"));       // "then..run this"
-              navigate("/");
+              axios.post("http://localhost:6001/users", newUser)  
+              .then(() => alert("User created!"));                
+              navigate("/"); // ==== ADD CORRECT ROUTE HERE ===  
           } 
 
           }
@@ -66,16 +47,6 @@ const Register = () => { // Three states - one for each input field
           alert ("Passwords did not match. Please try again.");
         }
   }
-      
-        // setUser({ // update to empty strings
-        //   username: "",
-        //   email: "",
-        //   password1: "",
-        //   password2: "",
-        // });
-  
-  
-
  
   return (
     <div className="container">
@@ -85,11 +56,9 @@ const Register = () => { // Three states - one for each input field
           <input
             type="text"
             placeholder="Username"
-            value={username}  // added value to each state
-            onChange={(e) => setUsername(e.target.value)} // added onChange to each state to be able to update the state (from input field)
+            value={username}  
+            onChange={(e) => setUsername(e.target.value)} // onChange to each state to be able to update the state (from input field)
             required
-            // onFocus={() => setUsernameFocus(true)} //does this add anything?
-            // onBlur={() => setUsernameFocus(false)} //does this add anything?
           />
         </label>
         <label>
@@ -111,10 +80,6 @@ const Register = () => { // Three states - one for each input field
           />
         </label>
         <label>
-          {/* // <span className={validMatch && matchPassword ? "valid" : "hide"}>
-          // </span>
-          // <span className={validMatch || !matchPassword ? "hide" : "invalid"}>
-          // </span> */}
           <input
             type="password"
             placeholder="Confirm Password"
@@ -132,33 +97,3 @@ const Register = () => { // Three states - one for each input field
 };
 
 export default Register;
-
-
-
-
-   //   const user = await axios
-    //   .get("/users")
-    //   .then((res) => checkEmail(res.data, email));
-
-    
-    // if (user) { // if email already exists
-    //   alert("User already exists!");
-    // } else {  // else save this new user - by making a post-request
-    //   const user = { username, email, password };
-    //   axios.post("/users", user).then(alert("User created!"));
-    //   navigate("../ProfilePage");
-    // } */
-
-
-
-    // const user = { username, email, password1, password2 };
-    //       axios
-    //           .post("http://localhost:6001/users", user) // "when this promise (API) is fullfilled.."
-    //           .then(() => alert("User created!"));       // "then.."
-      
-    //         setUser({ // update to empty strings
-    //           username: "",
-    //           email: "",
-    //           password1: "",
-    //           password2: "",
-    //         });
