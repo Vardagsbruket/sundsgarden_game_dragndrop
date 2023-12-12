c9ac83aebc4324a6d8e00e5088394b4e5043386d
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-12 10:55:11 +0100

    add first part for  profileDetails and error handling

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,3 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export const ProfileDetails = () => {
+  const [users, setUsers] = useState ([]);
+  useEffect(() => {
+    axios.get ('json server url')
+    .then(response=> {
+      setUsers(response.data);
+    })
+    .catch(error => { console.error('Oops, there is a problem fetching data:', error);
+  });
+  }, []);
+  
   return <div></div>;
 };
