35156fcbfd9207c6d8859f362cbddada6b564bbe
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-15 17:11:04 +0100

    remove profiledetails.jsx(not used), add welcome message after login on profilepage, remove not used code

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./Welcome.css";
 
-
-export const Welcome = ({ username }) => {
+export const Welcome = ({ }) => {
+  const userName = JSON.parse(localStorage.getItem("userName"))//get username from local storage, without the quotes
     return (
       <div>
-        <h1>Welcome, { username }!</h1>
+        <h1 className="Welcome">Welcome back, {userName}!</h1>
       </div>
     );
   };
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,43 +1,12 @@
-import { ProfileDetails } from "../components/ProfileDetails";
 import ProfileContent from '../components/Images/ProfileContent.png';
 import "./ProfilePage.css";
 import { Welcome } from "../components/Welcome";
-import React, { useState } from 'react';
-import Login from '../components/Login';
-
-
-
-export const LoginState = () => {
-  const [isLoggedIn, setIsLOggedIn] = useState(false);
-  const [loggedInUsername, setLoggedInUsername] = useState('');
-
-  const handleLogin = (username) => {
-    setIsLOggedIn(true);
-    setLoggedInUsername(username);
-  }
-
-  return (
-    <div>
-      {isLoggedIn ? (
-        <Welcome username={loggedInUsername} />
-      ) : (
-        <Login onLogin ={handleLogin} />
-      )}
-    </div>
-  );
-};
-
-
-
 
 export const ProfilePage = () => {
   return (
     <>
-      <ProfileDetails />
       <Welcome/>
       <img className="ProfileContent" src={ProfileContent}/>
-      
-
     </>
   );
 };
