d12415701df79e3edece6cb5f769d55bf906fd5e
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-21 23:52:07 +0100

    add useParams to ProfileDetails

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,28 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
 
 export const ProfileDetails = () => {
-  const [users, setUsers] = useState ([]);
+  const { userId } = useParams();
+  const { state } = useAuth();
+  const { user } = state;
+  const [users, setUsers] = useState(null);
+
   useEffect(() => {
-    axios.get ('json server url')
-    .then(response=> {
-      setUsers(response.data);
-    })
-    .catch(error => { console.error('Oops, there is a problem fetching data:', error);
-  });
+    axios
+      .get("json server url")
+      .then((response) => {
+        setUsers(response.data.users);
+      })
+      .catch((error) => {
+        console.error("Oops, there is a problem fetching data:", error);
+      });
   }, []);
 
   return (
-  <div>
-    <h1>User List</h1>
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>
-          <strong>Username:</strong> {user.username}<br />
-          <strong>Email:</strong> {user.email}
-        </li>
-      ))}
-    </ul>
-  </div>
+    <div>
+      <h1>User List</h1>
+      {users ? (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <strong>Username:</strong> {user.username}
+              <br />
+              <strong>Email:</strong> {user.email}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 };
